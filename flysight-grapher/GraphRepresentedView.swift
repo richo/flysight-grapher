a74a36f75eb1580ad331d0ffa0a4732b61f86b2a
Richo Healey <richo@psych0tik.net>
2019-07-08 16:53:09 -0700

    Maybe load things from CSV

--- flysight-grapher/GraphRepresentedView.swift
+++ flysight-grapher/GraphRepresentedView.swift
@@ -24,8 +24,8 @@ struct GraphRepresentedView: UIViewRepresentable {
         // Update the view.
     }
     
-    func loadDataFromCSV(data: Data) {
-        view.data = getData(25, range: 5)
+    func presentData(data: LineChartData) {
+        view.data = data
     }
 }
 
@@ -37,62 +37,8 @@ struct GraphView: View {
     }
     
     func loadDataFromCSV(_ url: URL) {
-        // TODO(richo) Errors
-        var data = try! Data(contentsOf: url)
-        self.graph.loadDataFromCSV(data: data)
-    }
-}
-
-func getData(_ count: Int, range: UInt32) -> LineChartData {
-    let yVals1 = (0..<count).map { (i) -> ChartDataEntry in
-        let mult = range / 2
-        let val = Double(arc4random_uniform(mult) + 50)
-        return ChartDataEntry(x: Double(i), y: val)
-    }
-    let yVals2 = (0..<count).map { (i) -> ChartDataEntry in
-        let val = Double(arc4random_uniform(range) + 450)
-        return ChartDataEntry(x: Double(i), y: val)
-    }
-    let yVals3 = (0..<count).map { (i) -> ChartDataEntry in
-        let val = Double(arc4random_uniform(range) + 500)
-        return ChartDataEntry(x: Double(i), y: val)
+        // TODO(richo) Deal with this error better
+        self.graph.presentData(data: loadAndParseDataFromCSV(url)!)
     }
     
-    let set1 = LineChartDataSet(entries: yVals1, label: "DataSet 1")
-    set1.axisDependency = .left
-    set1.setColor(UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1))
-    set1.setCircleColor(.white)
-    set1.lineWidth = 2
-    set1.circleRadius = 3
-    set1.fillAlpha = 65/255
-    set1.fillColor = UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)
-    set1.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
-    set1.drawCircleHoleEnabled = false
-    
-    let set2 = LineChartDataSet(entries: yVals2, label: "DataSet 2")
-    set2.axisDependency = .right
-    set2.setColor(.red)
-    set2.setCircleColor(.white)
-    set2.lineWidth = 2
-    set2.circleRadius = 3
-    set2.fillAlpha = 65/255
-    set2.fillColor = .red
-    set2.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
-    set2.drawCircleHoleEnabled = false
-    
-    let set3 = LineChartDataSet(entries: yVals3, label: "DataSet 3")
-    set3.axisDependency = .right
-    set3.setColor(.yellow)
-    set3.setCircleColor(.white)
-    set3.lineWidth = 2
-    set3.circleRadius = 3
-    set3.fillAlpha = 65/255
-    set3.fillColor = UIColor.yellow.withAlphaComponent(200/255)
-    set3.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
-    set3.drawCircleHoleEnabled = false
-    let data = LineChartData(dataSets: [set1, set2, set3])
-    data.setValueTextColor(.white)
-    data.setValueFont(.systemFont(ofSize: 9))
-    
-    return data
 }
