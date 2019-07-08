202b5084af9bd05196f8608a79ea52c5430569cd
Richo Healey <richo@psych0tik.net>
2019-07-08 11:12:23 -0700

    Initial Commit

---
+++ flysight-grapher/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  flysight-grapher
+//
+//  Created by richö butts on 7/8/19.
+//  Copyright © 2019 richö butts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
