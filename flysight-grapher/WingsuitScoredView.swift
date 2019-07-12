4ecfb76b9838ed17cf4634c394aabbdc7568756c
Richo Healey <richo@psych0tik.net>
2019-07-11 21:13:06 -0700

    Wingsuit scoring kinda works?

--- flysight-grapher/WingsuitScoredView.swift
+++ flysight-grapher/WingsuitScoredView.swift
@@ -8,66 +8,65 @@
 
 import SwiftUI
 import MapKit
+import Combine
 
 struct WingsuitScoredView : View {
-    var speed = ScoreView()
-    var time = ScoreView()
-    var distance = ScoreView()
     
-    var showInvalidRun = false
+    @ObjectBinding var scores: WingsuitScoreData = WingsuitScoreData()
     
     var body: some View {
         List {
-            if showInvalidRun {
+            if !scores.valid {
                 Section(header: Text("Warning")) {
                     Text("Loaded run does not contain a valid performance run")
                 }
-            }
-            Section(header: Text("Speed")) {
-                speed
-            }
-            Section(header: Text("Time")) {
-                time
-            }
-            Section(header: Text("Distance")) {
-                distance
+            } else {
+                Section(header: Text("Distance")) {
+                    ScoreView(score: scores.distance, unit: "m")
+                }
+                Section(header: Text("Time")) {
+                    ScoreView(score: scores.time, unit: "s")
+                }
+                Section(header: Text("Speed")) {
+                    ScoreView(score: scores.speed, unit: "m/s")
+
+                }
             }
         }.listStyle(.grouped)
     }
     
-    mutating func loadData(_ data: DataSet) {
+    func loadData(_ data: DataSet) {
         var state: WindowState = .BeforeEntry
         var entry: GateCrossing? = nil
         var exit: GateCrossing? = nil
         
-        for point in data.data {
+        outer: for point in data.data {
             switch state {
             case .BeforeEntry:
-                if point.altitude == 3000 {
+                if point.altitude < 3000 {
+                    print("Found the entry gate")
                     entry = GateCrossing(position: point.position, time: point.time)
                     state = .InWindow
                 }
             case .InWindow:
-                if point.altitude == 2000 {
+                if point.altitude < 2000 {
+                    print("Found the exit gate")
                     exit = GateCrossing(position: point.position, time: point.time)
                     state = .AfterExit
-                    break
+                    break outer
                 }
             case .AfterExit:
-                print("unreachable!")
-                break
+                break outer
             }
         }
         
         // TODO(richo) Expose something in the UI
         guard let entryGate = entry, let exitGate = exit else {
-            showInvalidRun = true
+            scores.invalidRun()
             return
         }
-        
-        speed.loadSpeed(entryGate, exitGate)
-        distance.loadDistance(entryGate, exitGate)
-        time.loadTime(entryGate, exitGate)
+
+        scores.validRun(entry: entryGate, exit: exitGate)
     }
     
     enum WindowState {
@@ -79,29 +78,25 @@ struct WingsuitScoredView : View {
 
 struct ScoreView: View {
     var score: Double?
+    var unit: String
+    
     var body: some View {
         guard let score = score else {
             return Text("No data")
         }
-        return Text("Score: \(score)")
+        return Text("\(score)\(unit)")
     }
-    
-    mutating func loadSpeed(_ entry: GateCrossing, _ exit: GateCrossing) {
+}
+
+struct WingsuitScorer {
+    func speed(_ entry: GateCrossing, _ exit: GateCrossing) -> Double {
         let distance = self.distance(entry, exit)
         let time = self.time(entry, exit)
         
-        score = distance / time
+        return distance / time
     }
-    
-    mutating func loadDistance(_ entry: GateCrossing, _ exit: GateCrossing) {
-        score = distance(entry, exit)
-    }
-    
-    mutating func loadTime(_ entry: GateCrossing, _ exit: GateCrossing) {
-        score = time(entry, exit)
-    }
-    
-    fileprivate func distance(_ entry: GateCrossing, _ exit: GateCrossing) -> Double {
+
+    func distance(_ entry: GateCrossing, _ exit: GateCrossing) -> Double {
         let entryPoint = MKMapPoint(entry.position)
         let exitPoint = MKMapPoint(exit.position)
         let distance = entryPoint.distance(to: exitPoint)
@@ -109,7 +104,7 @@ struct ScoreView: View {
         return distance
     }
     
-    fileprivate func time(_ entry: GateCrossing, _ exit: GateCrossing) -> Double {
+    func time(_ entry: GateCrossing, _ exit: GateCrossing) -> Double {
         let exitTime = exit.time
         let entryTime = entry.time
         
@@ -129,3 +124,45 @@ struct WingsuitScoredView_Previews : PreviewProvider {
     }
 }
 #endif
+
+// TODO(unify this with the swoop stuff
+final class WingsuitScoreData: BindableObject  {
+    let didChange = PassthroughSubject<WingsuitScoreData, Never>()
+    var scorer = WingsuitScorer()
+
+    
+    var valid = false {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    var distance: Double? = nil {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    var time: Double? = nil {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    var speed: Double? = nil {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    func validRun(entry: GateCrossing, exit: GateCrossing) {
+        self.distance = scorer.distance(entry, exit)
+        self.time = scorer.time(entry, exit)
+        self.speed = scorer.speed(entry, exit)
+        self.valid = true
+    }
+    
+    func invalidRun() {
+        self.valid = false
+        self.time = nil
+        self.speed = nil
+        self.distance = nil
+    }
+}
