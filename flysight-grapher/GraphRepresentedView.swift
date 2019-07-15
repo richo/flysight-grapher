319e7ed8e07a64a5fdd94ac9fdcb7a40d1bc0aad
Richo Healey <richo@psych0tik.net>
2019-07-14 18:21:02 -0700

    Hypothetically deal with really big flares

--- flysight-grapher/GraphRepresentedView.swift
+++ flysight-grapher/GraphRepresentedView.swift
@@ -13,6 +13,7 @@ import class SwiftCSV.CSV
 
 let MetersPerSecondToMilesPerHour = 2.23694
 let MetersToFeet = 3.28084
+let DEFAULT_RIGHT_AXIS_MINIMUM = -5.0
 
 struct GraphRepresentedView: UIViewRepresentable {
     var view: LineChartView = LineChartView()
@@ -30,7 +31,7 @@ struct GraphRepresentedView: UIViewRepresentable {
         
         view.rightAxis.labelTextColor = UIColor(named: "graphRightAxis")!
         view.rightAxis.gridLineWidth = 1.5
-        view.rightAxis.axisMinimum = -5
+        view.rightAxis.axisMinimum = DEFAULT_RIGHT_AXIS_MINIMUM
         
         view.legend.textColor = textColor
 
@@ -54,54 +55,57 @@ struct GraphView: View {
     }
     
     func loadData(_ data: DataSet) {
-        // TODO(richo) Deal with this error better
-        self.graph.presentData(data: transformData(data))
-    }
-}
-
-fileprivate func transformData(_ data: DataSet) -> LineChartData {
-    var hMSL: Array<ChartDataEntry> = []
-    var velX: Array<ChartDataEntry> = []
-    var velY: Array<ChartDataEntry> = []
-    
-    for point in data.data {
-        hMSL.append(ChartDataEntry(x: point.time, y: point.altitude * MetersToFeet))
-        velY.append(ChartDataEntry(x: point.time, y: point.vY() * MetersPerSecondToMilesPerHour))
+        var hMSL: Array<ChartDataEntry> = []
+        var velX: Array<ChartDataEntry> = []
+        var velY: Array<ChartDataEntry> = []
+        
+        var minVelX = 0.0
+        var minVelY = 0.0
+        
+        for point in data.data {
+            hMSL.append(ChartDataEntry(x: point.time, y: point.altitude * MetersToFeet))
+            velY.append(ChartDataEntry(x: point.time, y: point.vY() * MetersPerSecondToMilesPerHour))
+            velX.append(ChartDataEntry(x: point.time, y: point.vX() *
+                MetersPerSecondToMilesPerHour))
+            
+            if point.vY() < minVelY {
+                minVelY = point.vY()
+            }
+            if point.vX() < minVelX {
+                minVelX = point.vX()
+            }
+        }
+        
+        let alt = LineChartDataSet(entries: hMSL, label: "altitude")
+        alt.axisDependency = .left
+        let altitudeColor = UIColor(named: "graphAltitude")!
+        alt.setColor(altitudeColor)
+        alt.drawCirclesEnabled = false
+        alt.lineWidth = 2
+        
+        let vSpeed = LineChartDataSet(entries: velY, label: "v speed")
+        vSpeed.axisDependency = .right
+        let vSpeedColor = UIColor(named: "graphVerticalSpeed")!
+        vSpeed.setColor(vSpeedColor)
+        vSpeed.drawCirclesEnabled = false
+        vSpeed.lineWidth = 2
+        
+        let hSpeed = LineChartDataSet(entries: velX, label: "h speed")
+        hSpeed.axisDependency = .right
+        let hSpeedColor = UIColor(named: "graphHorizontalSpeed")!
+        hSpeed.setColor(hSpeedColor)
+        hSpeed.drawCirclesEnabled = false
+        hSpeed.lineWidth = 2
         
         
-        velX.append(ChartDataEntry(x: point.time, y: point.vX() *
-            MetersPerSecondToMilesPerHour))
+        let data = LineChartData(dataSets: [alt, vSpeed, hSpeed])
+        let valueTextColor = UIColor(named: "graphText")!
+        data.setValueTextColor(valueTextColor)
+        data.setValueFont(.systemFont(ofSize: 9))
+        
+        let min_axis_value = min(minVelY, minVelX)
+        
+        self.graph.view.rightAxis.axisMinimum = min(DEFAULT_RIGHT_AXIS_MINIMUM, min_axis_value)
+        self.graph.presentData(data: data)
     }
-    
-    let alt = LineChartDataSet(entries: hMSL, label: "altitude")
-    alt.axisDependency = .left
-    let altitudeColor = UIColor(named: "graphAltitude")!
-    alt.setColor(altitudeColor)
-    alt.drawCirclesEnabled = false
-    alt.lineWidth = 2
-    
-    let vSpeed = LineChartDataSet(entries: velY, label: "v speed")
-    vSpeed.axisDependency = .right
-    let vSpeedColor = UIColor(named: "graphVerticalSpeed")!
-    vSpeed.setColor(vSpeedColor)
-    vSpeed.drawCirclesEnabled = false
-    vSpeed.lineWidth = 2
-    
-    let hSpeed = LineChartDataSet(entries: velX, label: "h speed")
-    hSpeed.axisDependency = .right
-    let hSpeedColor = UIColor(named: "graphHorizontalSpeed")!
-    hSpeed.setColor(hSpeedColor)
-    hSpeed.drawCirclesEnabled = false
-    hSpeed.lineWidth = 2
-    
-    
-    let data = LineChartData(dataSets: [alt, vSpeed, hSpeed])
-    let valueTextColor = UIColor(named: "graphText")!
-    data.setValueTextColor(valueTextColor)
-    data.setValueFont(.systemFont(ofSize: 9))
-    
-    // TODO(richo)
-    // If there's a huge minimum in the dataset, reset the minimum value
-    
-    return data
 }
