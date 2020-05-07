06a6bac40cbc3c0071452ff4edc5adff8c0954fd
Richo Healey <richo@psych0tik.net>
2020-05-07 14:43:19 -0700

    try a diff coordinate system

--- flysight-grapher/AerobaticView.swift
+++ flysight-grapher/AerobaticView.swift
@@ -19,12 +19,12 @@ struct AerobaticView: View, DataPresentable {
         self.scene.presentData(converted)
         points = converted
     }
-    
+
     func clearData() {
 // TODO(richo)
     }
-    
-    
+
+
     var body: some View {
         self.scene
     }
@@ -42,35 +42,41 @@ struct Point3D {
 }
 
 private let EARTH_RADIUS = 6371.0
+// TODO(richo) The scale also needs to be worked out relative to altitude? We can *maybe* do something smart like at least normalise this to how far a given longitude line is
 private let SCALE = 0.1
 private func convertData(_ data: DataSet) -> Array<Point3D> {
     var offset_x = CGFloat(0.0)
     var offset_y = CGFloat(0.0)
     func convertPoint(_ point: DataPoint) -> Point3D {
-        var x = CGFloat(SCALE * EARTH_RADIUS * cos(point.position.latitude) * cos(point.position.longitude));
+      // We'll just naively lift everything into the positive domain and then treat them as being points on a large grid. Don't do aerobatics near the equator if you want accurate data.
+      // There's for sure a universe in which we use our complete understanding of the point + altitude to construct these points in 3d space, but for now this is enough to test the hypothesis.
+      var flat_x = point.position.longitude + 180
+      var flat_y = point.position.latitude + 180
+
+        var x = CGFloat(SCALE * flat_x);
         x -= offset_x;
-        var y = CGFloat(SCALE * EARTH_RADIUS * cos(point.position.latitude) * sin(point.position.longitude));
+        var y = CGFloat(SCALE * flat_y);
         y -= offset_y;
-        
+
         return Point3D(point: CGPoint(x: x, y: y), altitude: CGFloat(point.altitude))
     }
-    
+
     let first = convertPoint(data.data[0]);
     offset_x = first.point.x
     offset_y = first.point.y
-    
+
     return data.data.map(convertPoint)
 }
 
 struct ScenekitView : UIViewRepresentable {
     let scene = SCNScene(named: "art.scnassets/ship.scn")!
-    
+
     func presentData(_ data: Array<Point3D>) {
         for point in data {
             let sphere = SCNSphere(radius: 0.1)
             sphere.firstMaterial?.diffuse.contents = UIColor.systemPink
             let sphereNode = SCNNode(geometry: sphere)
-            sphereNode.position = SCNVector3(x: Float(point.point.x), y: Float(point.point.y), z: Float(point.altitude))
+            sphereNode.position = SCNVector3(x: Float(point.point.x), y: Float(point.altitude), z: Float(point.point.y))
             scene.rootNode.addChildNode(sphereNode)
         }
     }
@@ -97,13 +103,13 @@ struct ScenekitView : UIViewRepresentable {
         ambientLightNode.light!.type = .ambient
         ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
-        
+
         let sphere = SCNSphere(radius: 0.1)
         sphere.firstMaterial?.diffuse.contents = UIColor.red
         let sphereNode = SCNNode(geometry: sphere)
         sphereNode.position = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
         scene.rootNode.addChildNode(sphereNode)
-    
+
         // retrieve the SCNView
         let scnView = SCNView()
         return scnView
